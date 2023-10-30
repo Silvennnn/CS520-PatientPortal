cd716c596d05ef844211cbf3a5f07fb7e8010943
yk930608 <yk930608@gmail.com>
2023-10-30 13:39:50 -0400

    Update URL with .env file

--- server/database/database.py
+++ server/database/database.py
@@ -1,8 +1,22 @@
-from sqlalchemy import create_engine, String, Column
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, String, Column, URL
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://kai_ye:0Jm2fvuxpBAKbHflQiNALA@rough-quetzal-12554.7tt.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
+load_dotenv()
+
+host = os.environ.get("host")
+port = os.environ.get("port")
+username = os.environ.get("username")
+dbname = os.environ.get("dbname")
+password = os.environ.get("password")
+DATABASE_URL = URL.create(
+    "postgresql",
+    username=username,
+    password=password,  # plain (unescaped) text
+    host=host,
+    database=dbname,
+)
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
