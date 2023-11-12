b03b9cbf591a6312176ef4ebf3dfed8a6f92a90a
Silvennnn <xumingdeng@umass.edu>
2023-11-12 17:22:51 -0500

    Homepage UI implmentation

--- patient-health/app/page.tsx
+++ patient-health/app/page.tsx
@@ -1,113 +1,117 @@
 import Image from 'next/image'
 
+const navigation = [
+    // { name: 'PatientHealth', href: '#' }
+]
+
+
+//     <a href="#" className="-m-1.5 p-1.5">
+//     <span className="sr-only">PatientHealth</span>
+// <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-hexo"
+//      width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"
+//      fill="#009988" strokeLinecap="round" strokeLinejoin="round">
+//     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+//     <path
+//         d="M19.875 6.27c.7 .398 1.13 1.143 1.125 1.948v7.284c0 .809 -.443 1.555 -1.158 1.948l-6.75 4.27a2.269 2.269 0 0 1 -2.184 0l-6.75 -4.27a2.225 2.225 0 0 1 -1.158 -1.948v-7.285c0 -.809 .443 -1.554 1.158 -1.947l6.75 -3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98h-.033z"/>
+//     <path d="M9 8v8"/>
+//     <path d="M15 8v8"/>
+//     <path d="M9 12h6"/>
+// </svg>
+// </a>
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+    //   <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    //         Page
+    //   </div>
+    // </main>
+      <div className={"bg-white min-h-screen"}>
+          <header className="absolute inset-x-0 top-0 z-50">
+              <div className="mx-auto max-w-7xl">
+                  <div className="px-6 pt-6 lg:max-w-2xl lg:pl-8 lg:pr-0">
+                      <nav className="flex items-center justify-between lg:justify-start gap-x-2" aria-label="Global">
+                          <a href="#" className="-m-1.5 p-1.5">
+                              <span className="sr-only">PatientHealth</span>
+                              <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-hexo"
+                                    width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"
+                                    fill="#009988" strokeLinecap="round" strokeLinejoin="round">
+                                   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                   <path
+                                       d="M19.875 6.27c.7 .398 1.13 1.143 1.125 1.948v7.284c0 .809 -.443 1.555 -1.158 1.948l-6.75 4.27a2.269 2.269 0 0 1 -2.184 0l-6.75 -4.27a2.225 2.225 0 0 1 -1.158 -1.948v-7.285c0 -.809 .443 -1.554 1.158 -1.947l6.75 -3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98h-.033z"/>
+                                   <path d="M9 8v8"/>
+                                   <path d="M15 8v8"/>
+                                   <path d="M9 12h6"/>
+                               </svg>
+                           </a>
+                          <p className="font-mono text-teal-600 font-bold text-lg">PatientHealth</p>
+
+                          {/*<div className="hidden lg:ml-12 lg:flex lg:gap-x-14">*/}
+                          {/*    {navigation.map((item) => (*/}
+                          {/*        <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">*/}
+                          {/*            {item.name}*/}
+                          {/*        </a>*/}
+                          {/*    ))}*/}
+                          {/*</div>*/}
+                      </nav>
+                  </div>
+              </div>
+          </header>
+
+          <div className="relative min-h-screen">
+              <div className="mx-auto max-w-7xl">
+                  <div className="relative z-10 pt-14 lg:w-full lg:max-w-2xl">
+                      <svg
+                          className="absolute inset-y-0 right-8 hidden h-full w-80 translate-x-1/2 transform fill-white lg:block"
+                          viewBox="0 0 100 100"
+                          preserveAspectRatio="none"
+                          aria-hidden="true"
+                      >
+                          <polygon points="0,0 90,0 50,100 0,100" />
+                      </svg>
+
+                      <div className="relative px-6 py-32 sm:py-40 lg:px-8 lg:py-56 lg:pr-0">
+                          <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
+                              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                                  Looking for your patient portal?
+                              </h1>
+                              <p className="text-xl pt-10 font-bold tracking-tight text-black">
+                                  Manage your patient profile and make appointment <br /><span className={"text-teal-600 underline decoration-wavy "}>Anytime, Anywhere</span> with <span className={"text-teal-600 underline decoration-wavy "}>PatientHealth</span>
+                              </p>
+                              <div className="mt-5 flex items-center gap-x-6">
+                                  <a
+                                      href="#"
+                                      className="rounded-md bg-teal-600 px-3.5 py-2.5 text-md font-semibold text-white shadow-sm hover:bg-teal-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-700"
+                                  >
+                                      Register
+                                  </a>
+                                  <a href="/login" className="text-lg font-semibold leading-6 text-gray-900 hover:text-teal-600">
+                                      Login <span aria-hidden="true">→</span>
+                                  </a>
+                              </div>
+
+                              <p className="mt-10 text-lg leading-8 text-gray-600">
+                                  PatientHealth, the free app that allows you to:
+                                  <ul className="list-disc">
+                                      <li>Access your patient profile anytime, anywhere</li>
+                                      <li>View all your medical records online</li>
+                                      <li>Never worry about missing paper report</li>
+                                      <li>Make appointment with your doctors</li>
+                                      <li>And More ...</li>
+                                  </ul>
+                              </p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div className="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+                  <img
+                      className="aspect-[3/2] object-cover lg:aspect-auto lg:h-full lg:w-full"
+                      src="patient.pdf"
+                      alt=""
+                  />
+              </div>
+          </div>
+
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   )
 }
