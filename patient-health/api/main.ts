baebad4b6d16bc9bac3fcc9fa66cd685197378de
Gavin <gavin-duan@qq.com>
2023-12-04 14:26:02 -0500

    creating registerUser service, waiting for test

--- patient-health/api/main.ts
+++ patient-health/api/main.ts
@@ -1,11 +1,35 @@
 import Undici from "undici-types";
 import fetch = Undici.fetch;
+import {list} from "postcss";
+import {List} from "postcss/lib/list";
+import axios from "axios";
 
-export async function registerUser(user){
-    response = await fetch('/user/createUser/', {
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000',
+});
+
+export default api;
+
+interface User {
+  "account_name": string,
+  "password": string,
+  "account_type": number,
+  "first_name": string,
+  "last_name": string,
+  "middle_name": string,
+  "phone_number_number": string,
+  "date_of_birth": string,
+  "gender": string,
+  "address": List
+}
+
+export async function registerUser(user: User){
+    const response = await fetch('/user/createUser/', {
         method: 'POST',
         headers: {
-
-        }
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
     })
 }
