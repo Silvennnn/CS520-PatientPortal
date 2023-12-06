1f0afcda83a9a20b0c14913a3c518635ced6be0f
Gavin <gavin-duan@qq.com>
2023-12-06 16:49:31 -0500

    Added user.ts for function getUser(), implemented getUserByUUID

--- patient-health/api/profile.ts
+++ patient-health/api/profile.ts
@@ -22,24 +22,4 @@ export async function updateUserProfile(token: string, address: string[], phoneN
     }
 }
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlYzQ4ZTJmOS0wYTRjLTRkZDQtODA2Zi1iMTJkZDM4Y2FkMTUiLCJleHAiOjE3MDQ0MjU3Mzd9.G9IWNPZPdyTU9jCXkpWU2pwVw0wkLLYSQRB_PVbT8ZM"
-const acc_name = "doctor_1"
 
-export async function getUserByAccountName(account_name, token){
-     const response = await fetch(baseURL + `/user/getUserByAccountName/?account_name=${account_name}&token=${token}`,{
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-     if(!response.ok){
-         throw new Error(`HTTP error! Status: ${response.status}`);
-     }
-    else{
-        const data = await response.json();
-        return data;
-     }
-
-}
-
-getUserByAccountName(acc_name, token)
