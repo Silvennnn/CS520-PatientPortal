2e5e4bc8e86156069595560af25b34ad8b960ecd
Gavin <gavin-duan@qq.com>
2023-12-06 21:32:23 -0500

    Implement updateMedicalRecordByUUID

--- patient-health/api/record.ts
+++ patient-health/api/record.ts
@@ -9,6 +9,14 @@ export interface Record{
       "patient_account_name": string
 }
 
+
+export interface Update_form{
+      "symptom": string,
+      "diagnosis": string,
+      "Medication": string
+}
+
+
 export async function createMedicalRecord(token, record){
     const response = await fetch(baseURL + `/medicalRecord/createMedicalRecord/?token=${token}`, {
         method: "POST",
@@ -35,4 +43,29 @@ const rec: Record = {
 }
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1MTE0ZTU0Ny1kMzdjLTQzNWQtYmIwNC1mZWRmODI2MTkzMGIiLCJleHAiOjE3MDQ1MDYzNzZ9.9-o2S0oCr3QRjRcwjST2DQI5d-mHrVzhjmh6P-KLuvk"
 
-createMedicalRecord(token, rec);
+
+export async function updateMedicalRecordByUUID(token, record_uuid, update_form){
+    const response = await fetch(baseURL + `/medicalRecord/updateMedicalRecordByUUID/?token=${token}&medical_record_uuid=${record_uuid}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(update_form)
+    });
+    if(response.ok){
+        console.log("success");
+        return response.json();
+    } else {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+}
+
+const rec_uuid = "8fb61056-9528-40ee-945e-22290184c842"
+
+const form: Update_form = {
+      "symptom": "xasdas",
+      "diagnosis": "ysd",
+      "Medication": "zasd"
+}
+
+updateMedicalRecordByUUID(token, rec_uuid, form)
